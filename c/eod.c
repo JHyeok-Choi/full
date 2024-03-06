664e03e72c5661e505a040b8b31b8c4dbcc6808d
Chang558 <grab112@naver.com>
2024-03-06 11:56:27 +0900

    eod

--- c/eod.c
+++ c/eod.c
@@ -5,12 +5,12 @@ int eod(int x) {
 		printf("%d is even\n",x);
 	else
 		printf("%d is odd\n", x);
-		}
-
+}
 
 void main() {
 	int x;
 	printf("Input number : ");
 	scanf("%d", &x);
 	eod(x);
-	}
+}
+
